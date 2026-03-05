cc5115a2b1e7da5a630dac53b42f0e226dd6442c
jhuang165 <jeffreyhuang165@gmail.com>
2026-03-04 23:03:10 -0600

    hw6-3

--- puzzle.py
+++ puzzle.py
@@ -2,7 +2,7 @@ puzzle_easy_key = 8983
 puzzle_easy_misspell = "gadqens"
 
 puzzle_key = 747585522          # <- your found 9-digit key
-puzzle_misspell = "mispeld"      # <- the misspelled token exactly as in decoded text
+puzzle_misspell = "cirupmstances"      # <- the misspelled token exactly as in decoded text
 
 cnet_id = "jhuang165"
 nonce = "14948743"
